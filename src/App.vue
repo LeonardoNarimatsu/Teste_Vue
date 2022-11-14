ab7b3bbec0d7c94feea91e3fdf969933efb2e8e2
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-11-14 16:52:24 -0300

    Change Name

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template> 
-  <HelloWorld/>
+  <Form/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
