d0612e6f0c4cb02cb75975c51040caa8042ed35d
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-11-14 16:41:12 -0300

    avaliação técnica vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,226 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="cadastro_ingressante">
+    <div class="card">
+      <div class="title">Cadastro de Ingresantes</div>
+      <div class="form">
+        <a>Nome</a>     
+          <input type="text" class="input" />
+
+          <label id="course">Curso</label> 
+          <select for="course" v-model="curso" class="classic">
+            <option v-for="curso in cursos" :key="curso.cursoName" :value="curso.cursoName">
+              {{ curso.cursoName }}
+            </option>
+          </select>   
+          
+          <label id="state">Estado</label> 
+          <select for="state" @change="switchSelect($event)" v-model="estado" class="classic">
+            <option v-for="estado in estados" :key="estado.estadoName" :value="estado.estadoName" >
+              {{ estado.estadoName }}
+            </option>
+          </select>
+
+          <label id="city">Cidade</label> 
+          <select for="city" v-model="municipio" class="classic">
+            <option v-for="cidade in cidadesEscolhidas" :key="cidade.cidadeName" :value="cidade.cidadeName">
+              {{ cidade.cidadeName }}
+            </option>
+          </select>
+          
+          <div class="button">
+            <button class="voltar">Voltar</button>
+            <button class="gravar">Gravar</button>
+          </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'IndexPage',
+  data() {
+    return {
+      selected: "",
+      municipio:"",
+      cidadesEscolhidas:"",
+      cursos: [{"cursoName":"Matemática"}, {"cursoName":"Letras"}, {"cursoName":"Geografia"}],
+      estados: [{"estadoName":"São Paulo"}, {"estadoName":"Rio de Janeiro"}, {"estadoName":"Minas Gerais"}],
+      cidades: {
+        cidadeSP: [{"cidadeName":"Mogi das Cruzes"}, {"cidadeName":"Suzano"}, {"cidadeName":"Poá"}, {"cidadeName":"Guararema"}],
+        cidadeRJ: [{"cidadeName":"Angra dos Reis"}, {"cidadeName":"Niterói"}, {"cidadeName":"Itaboraí"}],
+        cidadeMG: [{"cidadeName":"Belo Horizonte"}, {"cidadeName":"Monte Azul"}, {"cidadeName":"Muzambinho"}],
+      }
+    };
+  },
+
+  methods: {
+    switchSelect(event) {
+      this.selected = event.target.value;
+      console.log(this.cidades)
+       if(this.selected =="São Paulo"){
+          this.cidadesEscolhidas = this.cidades.cidadeSP
+        }else if(this.selected =="Rio de Janeiro"){
+          this.cidadesEscolhidas = this.cidades.cidadeRJ
+        }else if(this.selected =="Minas Gerais"){
+         this.cidadesEscolhidas = this.cidades.cidadeMG
+        }
+      }  
+    }  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.form {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  max-width: 200px;
+  margin: 0 auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.cadastro_ingressante {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 16px);
+  align-items: center;
 }
-li {
+
+.input{  
+  background-color: white;
+  border: 2px solid rgb(22, 113, 187);
+  border-radius: 6px; 
   display: inline-block;
-  margin: 0 10px;
+  font: inherit;  
+  padding: 4px 2.8em 4px 4px;
+  margin: 0;      
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  margin-top: 5px;
+  margin-bottom: 10px;
 }
-a {
-  color: #42b983;
+
+.card{ 
+  max-width: 600px;
+  border: 2px solid rgb(22, 113, 187);
+  border-radius: 10px;
+  width: 250px;
+  overflow: hidden;
+}
+
+.gravar {
+	background-color:#319e4e;
+	border-radius:9px;
+	border:1px solid #203822;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:5px 5px;
+	text-decoration:none;
+	text-shadow:0px 0px 0px #788276;
+  width: 40%;
+}
+
+.gravar:hover {
+	background-color:#5cbf2a;
+}
+
+.gravar:active {
+	position:relative;
+	top:1px;
+}
+
+.voltar {
+	background-color:#d0d953;
+	border-radius:9px;
+	border:1px solid #a6ba34;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:5px 5px;
+	text-decoration:none;
+	text-shadow:0px 0px 0px #788276;
+  width: 35%;
+}
+
+.voltar:hover {
+	background-color:#759663;
+}
+
+.voltar:active {
+	position:relative;
+	top:1px;
+}
+
+.button {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px;
+}
+
+h2 {
+  color: #203822;
+}
+
+.title {
+  background-color:rgb(22, 113, 187); 
+  text-align: center;  
+  font-size: 24px;
+  color: white; 
+  padding: 8px;
+  margin-bottom: 8px;
+}
+
+select {
+  background-color: white;
+  border: 2px solid rgb(22, 113, 187);
+  border-radius: 6px; 
+  display: inline-block;
+  font: inherit;  
+  padding: 4px 2.8em 4px 4px;
+  margin: 0;      
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+select.classic {
+  background-image:
+    linear-gradient(45deg, transparent 50%, white 50%),
+    linear-gradient(135deg, white 50%, transparent 50%), 
+    radial-gradient(rgb(22, 113, 187), rgb(22, 113, 187));
+    /* linear-gradient(to right,rgb(22, 113, 187), rgb(22, 113, 187)); */
+    
+  background-position: 
+    calc(100% - 14px) 50%,
+    calc(100% - 7px) 50%,
+    calc(100% - 4px) 50%;
+  background-size: 
+    7px 7px,
+    7px 7px,
+    20px 20px;
+  background-repeat: no-repeat;
+}
+
+select.classic:after {
+  content: " - Remember this";
+  background-color: yellow;
+  color: red;
+  font-weight: bold;
 }
 </style>
